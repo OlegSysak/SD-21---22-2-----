13beec2ba8334a9731396b2ea2d094fcd14a7fcd
Oleg Sysak <oleh.r.sysak@ukd.edu.ua>
2025-02-09 19:58:00 +0200

    Add my line

--- first_task.py
+++ first_task.py
@@ -1,2 +1,3 @@
 print("Hi, my name is Oleh Sysak Ruslanovych and I’m studying software development in college")
 print("Hi, my name is Volosovskyi Stepan Ivanovych and I’m studying software development in college")
+print("Task completed")
