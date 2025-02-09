22afe93299f5bc9f65d3e89b12f5ea4601eaa311
D1getal <121261654+D1getal@users.noreply.github.com>
2025-02-09 19:32:35 +0200

    Add my line

--- first_task.py
+++ first_task.py
@@ -1,1 +1,2 @@
 print("Hi, my name is Oleh Sysak Ruslanovych and I’m studying software development in college")
+print("Hi, my name is Volosovskyi Stepan Ivanovych and I’m studying software development in college")
